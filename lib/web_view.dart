4e320e42e9d7f8501f749bdf5093baebc3c3c6fc
Monjur Ahmed Akash <monjur3069@gmail.com>
2024-01-26 15:34:02 +0600

    initial commit

--- lib/web_view.dart
+++ lib/web_view.dart
@@ -15,31 +15,75 @@ class _WebViewState extends State<WebView> {
   @override
   void initState() {
     controller = WebViewController()
-      ..loadRequest(Uri.parse('https://www.google.com/'))
-    ;
+      ..loadRequest(Uri.parse('https://youngcastbd.com/'));
     controller
-      ..setNavigationDelegate(NavigationDelegate(
-            onPageStarted: (url) {setState(() {loadingPercentage = 0;});},
-            onProgress: (progress) {setState(() {
-              loadingPercentage = progress;
-            });},
-            onPageFinished: (url) {setState(() {loadingPercentage = 100;});}))
-    ..setJavaScriptMode(JavaScriptMode.unrestricted)
-    ..addJavaScriptChannel("Snacbar", onMessageReceived: (message){
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message.message)));
-    })
-    ;
+      ..setNavigationDelegate(NavigationDelegate(onPageStarted: (url) {
+        setState(() {
+          loadingPercentage = 0;
+        });
+      }, onProgress: (progress) {
+        setState(() {
+          loadingPercentage = progress;
+        });
+      }, onPageFinished: (url) {
+        setState(() {
+          loadingPercentage = 100;
+        });
+      }))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..addJavaScriptChannel("Snacbar", onMessageReceived: (message) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(message.message)));
+      });
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Web View'),
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                  icon: Icon(Icons.arrow_back_ios),
+                  onPressed: () async {
+                    final messenger = ScaffoldMessenger.of(context);
+                    if (await controller.canGoBack()) {
+                      await controller.goBack();
+                    } else {
+                      messenger.showSnackBar(
+                          SnackBar(content: Text('No Back History Found')));
+                      return;
+                    }
+                  }),
+              IconButton(
+                  icon: Icon(Icons.arrow_forward_ios),
+                  onPressed: () async {
+                    final messenger = ScaffoldMessenger.of(context);
+                    if (await controller.canGoForward()) {
+                      await controller.goForward();
+                    } else {
+                      messenger.showSnackBar(
+                          SnackBar(content: Text('No Forward History Found')));
+                      return;
+                    }
+                  }),
+              IconButton(
+                  icon: Icon(Icons.replay),
+                  onPressed: () async {
+                    controller.reload();
+                  }),
+            ],
+          )
+        ],
+      ),
       body: SafeArea(
         child: Stack(
           children: [
             WebViewWidget(controller: controller),
-            if(loadingPercentage<100)
+            if (loadingPercentage < 100)
               LinearProgressIndicator(
                 value: loadingPercentage / 100.0,
               )
